2d4a0178798da050368d2efba4a73d8c3ee6cc58
Basmala Ahmed <133245850+basmalaa-ahmed@users.noreply.github.com>
2024-03-07 17:57:01 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict'
+const modal =document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal')
+const btnsOpenModal = document.querySelectorAll('.show-modal')
+console.log(btnsOpenModal);
+for(let i=0;i<btnsOpenModal.length;i++){
+    btnsOpenModal[i].addEventListener('click',
+    function(){
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden')
+    });
+
+    
+}
+const closeBtn = function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden')
+}
+btnCloseModal.addEventListener('click',closeBtn);
+    overlay.addEventListener('click',closeBtn
+)
+document.addEventListener('keydown',function(e){
+    if(e.key==='Escape'  && !modal.classList.contains('hidden')){
+       closeBtn()
+    }
+})
